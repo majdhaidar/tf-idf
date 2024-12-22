4a6bfe8f53e00f6aa8898ddec43a8aa0f34cb94c
mhaidar <mhaidar@areeba.com>
2024-12-22 11:49:43 +0200

    initial commit

---
+++ src/main/java/org/documentterm/App.java
@@ -0,0 +1,13 @@
+package org.documentterm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
